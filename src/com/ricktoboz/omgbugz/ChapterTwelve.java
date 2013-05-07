90fd75adcf1c02570da19c4b2b0e752381bc61f7
rick <loki.tr@gmail.com>
2013-05-06 21:35:22 -0500

    Almost working maxInRange method.

--- src/com/ricktoboz/omgbugz/ChapterTwelve.java
+++ src/com/ricktoboz/omgbugz/ChapterTwelve.java
@@ -9,9 +9,11 @@ package com.ricktoboz.omgbugz;
  */
 public class ChapterTwelve {
     public static void main(String[] args) {
-        int a = 2;
-        a *= 3;
-        System.out.println(a);
+        int[] test = new int[10];
+        for (int i = 0; i < test.length; i++) {
+            test[i] = i;
+        }
+        System.out.println("The max value is: " + maxInRange(test, 1, 4));
     }
 
     public static int[] cloneArray(int[] a) {
@@ -84,5 +86,135 @@ public class ChapterTwelve {
         return -1;
     }
 
+    public static int maxInRange(int[] range, int lowIndex, int highIndex) {
+        if (range.length == 0) {
+            {
+                System.out.println("There are no numbers in range.");
+                return -2147483648;
+            }
+            if (range.length == 1) {
+                return range[0];
+            }
+            int candidates = 0;
+            for (int i = 0; i < range.length; i++) {
+                if (range[i] <= highIndex && range[i] >= lowIndex) {
+                    candidates++;
+                }
+            }
+            if (candidates == 2) {
+                int[] bracket = new int[2];
+                int tracker = 0;
+                for (int i = 0; i < range.length; i++) {
+                    if (range[i] <= highIndex && range[i] >= lowIndex) {
+                        bracket[tracker] = range[i];
+                        tracker++;
+                    }
+                }
+                int winnerof2 = 0;
+                for (int j = 0; j < bracket.length; j++) {
+                    if (range[j] >= range[j + 1]) {
+                        winnerof2 = range[j];
+                    } else {
+                        winnerof2 = range[j + 1];
+                    }
+                }
+                return winnerof2;
+            }
+            if ((candidates % 2) == 0) {
+                int[] bracket1 = new int[(candidates / 2)];
+                int[] bracket2 = new int[(candidates / 2)];
+                int tracker = 0;
+                for (int i = 0; i == (candidates / 2); i++) {
+                    if (range[i] <= highIndex && range[i] >= lowIndex) {
+                        bracket1[tracker] = range[i];
+                        tracker++;
+                    }
+                }
+                tracker = 0;
+                for (int j = 0; j == (candidates / 2); j++) {
+                    if (range[j + (candidates / 2)] <= highIndex && range[j + (candidates / 2)] >= lowIndex) {
+                        bracket2[tracker] = range[j + (candidates / 2)];
+                        tracker++;
+                    }
+                }
+                if (maxInRange(bracket1, lowIndex, highIndex) >= (maxInRange(bracket2, lowIndex, highIndex))) {
+                    return maxInRange(bracket1, lowIndex, highIndex);
+                } else return maxInRange(bracket2, lowIndex, highIndex);
+            }
+            if ((candidates % 2) != 0) {
+                int[] bracket1 = new int[(candidates / 2)];
+                int[] bracket2 = new int[(candidates / 2) + 1];
+                int tracker = 0;
+                for (int i = 0; i == (candidates / 2); i++) {
+                    if (range[i] <= highIndex && range[i] >= lowIndex) {
+                        bracket1[tracker] = range[i];
+                        tracker++;
+                    }
+                }
+                tracker = 0;
+                for (int j = 0; j == (candidates / 2); j++) {
+                    if (range[j + (candidates / 2)] <= highIndex && range[j + (candidates / 2)] >= lowIndex) {
+                        bracket2[tracker] = range[j + (candidates / 2)];
+                        tracker++;
+                    }
+                }
+                if (maxInRange(bracket1, lowIndex, highIndex) >= (maxInRange(bracket2, lowIndex, highIndex))) {
+                    return maxInRange(bracket1, lowIndex, highIndex);
+                } else return maxInRange(bracket2, lowIndex, highIndex);
+            }
+        }
 
+        /*/int numberOfCandidates = 0;
+        int candidateSlot = 0;
+        for (int i = 0; i < range.length; i++){
+            if (range[i] <= highIndex && range[i] >= lowIndex){numberOfCandidates++;}}
+        int[] candidates = new int[numberOfCandidates];
+        for (int i = 0; i < range.length; i++){
+            if (range[i] <= highIndex && range[i] >= lowIndex){candidates[candidateSlot] = range[i]; candidateSlot++;}}
+
+        if (candidates.length == 0){System.out.println("There are no numbers in range."); return 0;}
+        if (candidates.length == 1){return candidates[0];}
+        if (candidates.length == 2){
+            if (candidates[0] >= candidates[1]){return candidates[0];}
+            else return candidates[1];}
+        else {
+            int[] x = new int[(candidates.length/2)];
+            int[] y = new int[(candidates.length/2)];
+            for (int i = 0; i < x.length; i++){
+                x[i] = candidates[i];}
+            for (int i = 0; i < y.length; i++){
+                y[i] = candidates[i + (x.length - 1)];}
+            return maxInRange(x, )
+
+            int[] x = new int[(range.length - 2)];
+        for (int i = 0; (i + 2) < range.length; i++){
+            x[(i)] = range[(i + 2)];}
+
+        return maxInRange(x, lowIndex, highIndex);}
+}
+        }
+    }
+
+
+        if (range.length == 1){
+            if (range[0] >= lowIndex && range[0] <= highIndex){return range[0];}
+            else return 0;}
+
+        else if (range.length == 2){
+            if (range[0] >= lowIndex && range[0] <= highIndex){
+                if (range[1] >= lowIndex && range[1] <= highIndex){
+                    if (range[0] >= range[1]){return range[0];}
+                    else return range[1];}
+                else return range[0];}
+            else return 0;}
+
+        else {
+            int[] x = new int[(range.length - 2)];
+            for (int i = 0; (i + 2) < range.length; i++){
+                x[(i)] = range[(i + 2)];}
+
+            return maxInRange(x, lowIndex, highIndex);}
+    }
+/*/
+    }
 }
